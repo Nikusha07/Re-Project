d400f78eb015ea3bc721b4d96aed7881f3c5bcd3
Nukusha07 <nikatefnadze3@gmail.com>
2023-06-21 15:29:53 +0400

    add new products burger bar and become own website responsive

--- script.js
+++ script.js
@@ -24,3 +24,52 @@ function myFunction() {
       y.style.display = "none"
     }
   }
+
+
+
+var rotatingDiv = document.getElementById('rootbox');
+var rotation = 0;
+function rotateDiv() {
+  rotation += 90;
+
+  // Apply the rotation using CSS transform property
+  rotatingDiv.style.transform = 'rotate(' + rotation + 'deg)';
+}
+window.addEventListener('scroll', e => {
+  var el = document.getElementById('jsScroll');
+  if(window.scrollY > 200) {
+    el.classList.add('visible');
+  } else {
+    el.classList.remove('visible');
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+function openCloseFunction() {
+  var x = document.getElementById("burger")
+  if (x.style.display == "block") {
+    x.style.display = "none";
+    
+  } else {
+    x.style.display = "flex" ;   
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("search")
+  var z = document.getElementById("burger")
+  if (x.style.display == "flex") {
+    x.style.display = "none"
+    x.style.height= "0px"
+    z.style.display="none"
+  } else {
+    x.style.display = "flex" ; 
+    x.style.height = "400px";
+    z.style.display="none"
+  }
+}
